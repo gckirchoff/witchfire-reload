6072d40eb33097419cb336db9b19da343afdded5
gckirchoff <gckirchoff@gmail.com>
2024-12-25 14:54:16 -0500

    Initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import '../app.css';
+
+	let { children } = $props();
+</script>
+
+<div class="app">
+	{@render children()}
+</div>
+
+<style>
+	.app {
+		min-height: 100vh;
+		background-color: #1b1f20;
+	}
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+</script>
+
+<main>
+	<div class="headers">
+		<h1>Frostbite Reload</h1>
+		<h3>Press R to reload</h3>
+	</div>
+	<div class="bar"></div>
+</main>
+
+<style>
+	main {
+		min-height: inherit;
+		padding-top: 30vh;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		gap: 3rem;
+		text-align: center;
+	}
+
+	.bar {
+		background-color: blue;
+		width: min(30rem, 90%);
+		height: 1.5rem;
+	}
+</style>
